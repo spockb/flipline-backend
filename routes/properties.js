726fc138023218eb0ec48ff0ea7ca410fd65a1e3
spockb <178428622+spockb@users.noreply.github.com>
2025-08-09 15:16:34 -0700

    fixed DELETE and PUT

--- routes/properties.js
+++ routes/properties.js
@@ -174,7 +174,7 @@ router.put("/:id", async (req, res) => {
 router.delete("/:id", async (req, res) => {
   const id = Number(req.params.id);
   if (!Number.isInteger(id))
-    return res.status(404).json({ error: "Invalid ID" });
+    return res.status(400).json({ error: "Invalid ID" });
 
   try {
     const deletedProperty = await prisma.property.delete({ where: { id } });
